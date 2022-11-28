e464d84a9ce4f3be27c776ff37e544e82faf4d43
Will Fields <will.openfields@gmail.com>
2022-11-28 05:50:44 -0600

    changed to just pull final score, not necessarily from regulation

--- nhl/regulationscore.r
+++ nhl/regulationscore.r
@@ -2,15 +2,16 @@ regulationscore <- function(game){
 # goal tabulation to get score at end of regulation
 
 # get event type = goal
-which(game$event=="Goal") -> goals
+#which(game$event=="Goal") -> goals
 
 # reg goals
-which(game$period[goals] <= 3) -> rgoals
-lastgoal <- length(rgoals)
+#which(game$period[goals] <= 3) -> rgoals
+#lastgoal <- length(rgoals)
 
 #lastgoal <- goals[max(rgoals)]
-
-return(as.data.frame(c(game$home_name[lastgoal], game$away_name[lastgoal], game$home_score[lastgoal], game$away_score[lastgoal])))
+length(game$home_final) -> events
+return(as.data.frame(c(game$home_name[events], game$away_name[events], game$home_final[events], game$away_final[events])))
 
 return(score)
 }
+
