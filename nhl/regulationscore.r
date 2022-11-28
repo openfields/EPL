7813cd2bb8f78bf282fc195122303d4f01f13dbc
Will Fields <will.openfields@gmail.com>
2022-11-28 05:28:56 -0600

    changed way to determine score

--- nhl/regulationscore.r
+++ nhl/regulationscore.r
@@ -6,7 +6,9 @@ which(game$event=="Goal") -> goals
 
 # reg goals
 which(game$period[goals] <= 3) -> rgoals
-lastgoal <- goals[max(rgoals)]
+lastgoal <- length(rgoals)
+
+#lastgoal <- goals[max(rgoals)]
 
 return(as.data.frame(c(game$home_name[lastgoal], game$away_name[lastgoal], game$home_score[lastgoal], game$away_score[lastgoal])))
 
